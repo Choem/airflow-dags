e1f5a6371cde3596be70dce4d86b47fec245f342
Chiem Janssen <chiemjanssen@home.nl>
2021-06-18 12:04:20 +0200

    Updated dag

--- images/train_and_save_personal_model/src/main.py
+++ images/train_and_save_personal_model/src/main.py
@@ -39,12 +39,12 @@ class SaveModelError(Exception):
 
 # Functions
 def get_graphql_client():
-        transport = RequestsHTTPTransport(
-            url='http://file-service:4000/file/graphql/query', 
-            use_json=True,
-        )
+    transport = RequestsHTTPTransport(
+        url='http://file-service:4000/file/graphql/query', 
+        use_json=True,
+    )
 
-       return Client(transport=transport, fetch_schema_from_transport=True)
+    return Client(transport=transport, fetch_schema_from_transport=True)
 
 def download_logs(patient_id, client):
     # Set params
