b91d62d18e83b6adea1308eec2d16bbe1db9e361
Chiem Janssen <chiemjanssen@home.nl>
2021-06-18 14:20:36 +0200

    Updated dag

--- images/train_and_save_personal_model/src/main.py
+++ images/train_and_save_personal_model/src/main.py
@@ -9,8 +9,7 @@ from datetime import datetime
 from sklearn import datasets
 from sklearn.svm import SVC
 from gql import gql, Client
-from gql.transport.requests import RequestsHTTPTransport
-
+from gql.transport.aiohttp import AIOHTTPTransport
 
 # Queries
 get_patient_logs_query = gql("""
@@ -41,9 +40,8 @@ class SaveModelError(Exception):
 
 # Functions
 def get_graphql_client():
-    transport = RequestsHTTPTransport(
+    transport = AIOHTTPTransport(
         url='http://file-service:4000/file/graphql/query', 
-        use_json=True,
     )
 
     return Client(transport=transport, fetch_schema_from_transport=True)
