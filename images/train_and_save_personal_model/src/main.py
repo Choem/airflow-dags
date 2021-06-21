fb93a601accf0b6f85fe9e3c1410e20d6e46f70f
Chiem Janssen <chiemjanssen@home.nl>
2021-06-21 12:01:32 +0200

    Updated dag

--- images/train_and_save_personal_model/src/main.py
+++ images/train_and_save_personal_model/src/main.py
@@ -49,6 +49,7 @@ def get_minio_client():
         "minio:9000",
         access_key=os.environ['MINIO_ACCESS_KEY'],
         secret_key=os.environ['MINIO_SECRET_KEY'],
+        secure=False
     )
 
 def download_logs(patient_id, graphql_client, minio_client):
